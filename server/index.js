e0fe1d4b10aa7feb5a15677a78ba03d38d950b5c
Paras Singh <146059706+prsSng383@users.noreply.github.com>
2025-07-15 17:13:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000 ;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://mern-authentication-system-backend-5nj7.onrender.com']
 
 app.use(express.json());
 app.use(cookieParser());
