9686351638255e72decc9dc56fcfad98511d303f
Paras Singh <146059706+prsSng383@users.noreply.github.com>
2025-07-15 18:10:59 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,8 +40,6 @@ export const register = async (req , res) => {
          httpOnly : true,
          secure: true,
          sameSite:"none",
-         path: "/",
-         domain: ".onrender.com",
          maxAge: 7 * 24 * 60 * 60 * 1000
       })
 
@@ -91,8 +89,6 @@ export const login = async (req, res) =>{
          httpOnly : true,
          secure: true,
          sameSite:"none",
-         path: "/",
-         domain: ".onrender.com",
          maxAge: 7 * 24 * 60 * 60 * 1000
       })
       
